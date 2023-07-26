d59dc14fa41f92aead86df95efbe75743200410a
swoolee97 <swoolee97@ajou.ac.kr>
2023-07-26 17:32:37 +0900

    asdf

---
+++ ios/testProject2-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  testProject2-Bridging-Header.h
+//  testProject2
+//
+//  Created by seung-u on 7/26/23.
+//
+
+#ifndef testProject2_Bridging_Header_h
+#define testProject2_Bridging_Header_h
+#import "RCTBridgeModule.h"
+#import "RCTBridge.h"
+#import "RCTEventDispatcher.h"
+#import "RCTRootView.h"
+#import "RCTUtils.h"
+#import "RCTConvert.h"
+
+#endif /* testProject2_Bridging_Header_h */
