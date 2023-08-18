2e6cf018ad5725437eadb36130cd3a42f490cae2
조성주 <joseongju@joseongjuui-MacBookAir.local>
2023-08-18 17:58:23 +0900

    update

--- Screen/AuthStackScreens/ForgotPasswordScreen.js
+++ Screen/AuthStackScreens/ForgotPasswordScreen.js
@@ -1,40 +1,73 @@
 import React, { useState } from 'react';
 import { SafeAreaView, View, Text, TextInput, StyleSheet, Alert, TouchableOpacity, ScrollView } from 'react-native';
 import PreURL from '../../PreURL/PreURL';
+
 const ForgotPasswordScreen = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [verificationCode, setVerificationCode] = useState('');
     const [generatedCode, setGeneratedCode] = useState('');
     const [codeSent, setCodeSent] = useState(false);
     const [emailValid, setEmailValid] = useState(null);
-    const [checkCode, setCheckCode] = useState(false);
+    const [checkCode,setCheckCode] = useState(false);
 
 
     const emailRegEx = /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/i;
 
-    const handleRequestReset = async () => {
-        if (!codeSent) {
-            Alert.alert('오류', '이메일 인증을 먼저 완료해주세요.');
+    const emailCheck = (inputEmail) => {
+        setEmail(inputEmail);
+        setEmailValid(emailRegEx.test(inputEmail));
+    }
+    const createRandomCode = () => {
+        return (String(Math.floor(Math.random() * 1000000)).padStart(6, "0"));
+    }
+
+    const checkRandomCode = () => {
+        if (verificationCode === generatedCode) {
+            setCodeSent(true);
+            Alert.alert('성공', '인증번호가 확인되었습니다.', [
+                {
+                    text: '확인',
+                    onPress: () => navigation.navigate('PasswordResetScreen')
+                }
+            ]);
+        } else {
+            Alert.alert('오류', '인증번호가 일치하지 않습니다.');
+        }
+    }
+
+    const emailAuthentication = async() => {
+        if (!emailValid) {
+            Alert.alert('오류', '유효한 이메일 주소를 적어주세요.');
             return;
         }
-        try {
-            const response = await fetch(PreURL.preURL+'/api/emailAuth', {
-                // ... other fetch settings ...
-            });
-
-            const data = await response.json();
-
-            if (data.success) {
-                Alert.alert('성공', '비밀번호 재설정 메일이 전송되었습니다.');
-                navigation.navigate('PasswordResetScreen');
-            } else {
-                Alert.alert('오류', data.message || '메일 전송 실패');
-            }
-        } catch (error) {
-            console.error(error);
+        const code = createRandomCode();
+        setGeneratedCode(code);
+        let dataToSend = { user_email: email, randomCode: code };
+        let formBody = [];
+        for (let key in dataToSend) {
+            let encodedKey = encodeURIComponent(key);
+            let encodedValue = encodeURIComponent(dataToSend[key]);
+            formBody.push(encodedKey + '=' + encodedValue);
         }
-    };
-    
+        formBody = formBody.join('&');
+        fetch(PreURL.preURL + '/api/emailAuth', {
+            method: 'POST',
+            body: formBody,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            },
+        })
+        .then((response) => response.json())
+        .then((responseJson) => {
+            if (responseJson.success) {
+                Alert.alert('성공', '인증 메일이 전송되었습니다.');
+            } else {
+                Alert.alert('오류', responseJson.message || '메일 전송 실패');
+            }
+        })
+        .catch(error => console.error(error));
+    }
+
     const styles = StyleSheet.create({
         container: {
             flex: 1,
