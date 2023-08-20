73d1de4cde9118d5559915daf048c68d78ffb477
swoolee97 <swoolee97@ajou.ac.kr>
2023-08-20 23:50:06 +0900

    기프티콘 검색

--- Screen/GifticonListScreens/SearchingScreen.js
+++ Screen/GifticonListScreens/SearchingScreen.js
@@ -1,64 +1,89 @@
 import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Alert, ActivityIndicator, RefreshControl, StyleSheet, Image } from "react-native"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { FlatList } from "react-native"
-import SearchingResultScreen from "./SearchingResultScreen"
 import PreURL from "../../PreURL/PreURL"
 
-const SearchingScreen = ({navigation}) => {
+const SearchingScreen = ({ navigation }) => {
     const [gifts, setGifts] = useState([]);
     const [keyword, setKeyword] = useState('')
     const [page, setPage] = useState(1)
     const [hasMore, setHasMore] = useState(true)
     const [refreshing, setRefreshing] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [firstKeyword, setFirstKeyword] = useState('');
 
-    const handleSearchPress = async () => {
-        if(keyword.length < 2){
+    // 찾기버튼을 눌렀을 땐 false.   isSearchButton == 찾기 눌렀을 때 true
+    const handleSearchPress = async (isSearchButton = true) => {
+        if (isSearchButton && keyword.length < 2) {
             return Alert.alert('두글자 이상 입력해주세요')
         }
-        const response = await fetch(PreURL.preURL+`/api/gifticon/search?keyword=${keyword}&page=${page}`);
-        const data = await response.json()
-        if(page == 1){
-            setGifts(data.gifticons)
-        }else{
-            setGifts(prevGifts => [...prevGifts,...data.gifticons])
+        if (isSearchButton) {
+            setPage(1)
+            setHasMore(true)
+            setGifts([])
+            setFirstKeyword(keyword)
+        }
+        // 받아올 데이터가 더 없는데 추가데이터를 불러오려고 할 때 return
+        if (!hasMore && !isSearchButton) {
+            console.log('뭔가 이상함')
+            return;
+        }
+        try {
+            const fetchKeyword = isSearchButton ? keyword : firstKeyword;
+            const nextPage = isSearchButton ? 1 : page + 1;
+            const response = await fetch(`${PreURL.preURL}/api/gifticon/search?keyword=${fetchKeyword}&page=${nextPage}`)
+            const data = await response.json()
+            setGifts(prevGifts => [...prevGifts, ...data.gifticons])
+            setHasMore(data.hasMore)
+            console.log(data.gifticons)
+        } catch (error) {
+            console.error(error)
         }
     }
     return (
         <SafeAreaView>
-            <Text>서칭 메인 스크린</Text>
             <View style={{ flexDirection: 'row' }}>
-                <TextInput onChangeText={setKeyword} placeholder='기프티콘'></TextInput>
+                <TextInput onChangeText={setKeyword} placeholder='검색어'></TextInput>
                 <TouchableOpacity onPress={() => {
-                    handleSearchPress();
+                    handleSearchPress(true);
                 }}>
                     <Text>찾기</Text>
                 </TouchableOpacity>
             </View>
-            <FlatList
-                data={gifts}
-                numColumns={2}
-                keyExtractor={(item) => item._id}
-                renderItem={({ item }) => (
-                    <TouchableOpacity onPress = {() => handleSearchPress(item)}>
-                        <View style={styles.listItem}>
-                            <Image source={{ uri: item.url }} style={styles.image} />
-                            <Text style={styles.itemName}>{item.gifticon_name}</Text>
-                            <Text>{item.price}원</Text>
-                        </View>
-                    </TouchableOpacity>
-                )}
-                // onEndReached={}
-                onEndReachedThreshold={0.8} // 리스트의 80% 스크롤하면 추가 데이터 로드
-                ListFooterComponent={() => isLoading ? <ActivityIndicator size="small" color="#0000ff" /> : null}
-                showsVerticalScrollIndicator={false}
-                refreshControl={
-                    <RefreshControl
-                        refreshing={refreshing}
-                        // onRefresh={onRefresh}
-                    />
-                }
-            />
+            <View>
+                
+                <FlatList
+                    data={gifts}
+                    numColumns={2}
+                    keyExtractor={(item) => item._id}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity >
+                            <View style={styles.listItem}>
+                                <Image source={{ uri: item.url }} style={styles.image} />
+                                <Text style={styles.itemName}>{item.gifticon_name}</Text>
+                                <Text>{item.price}원</Text>
+                            </View>
+                        </TouchableOpacity>
+                    )}
+                    onEndReached={() => {
+                        if (hasMore) {
+                            console.log('추가 데이터 불러오기')
+                            handleSearchPress(false);
+                        }
+                    }}
+                    onEndReachedThreshold={1.0} // 리스트의 80% 스크롤하면 추가 데이터 로드
+                    ListFooterComponent={() => isLoading ? <ActivityIndicator size="small" color="#0000ff" /> : null}
+                    showsVerticalScrollIndicator={false}
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={refreshing}
+                            onRefresh={() => {
+                                
+                            }}
+                        />
+                    }
+                />
+            </View>
         </SafeAreaView>
     )
 }
