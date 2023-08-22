70e81d90428c25d41b1a14d2b7b638e657d94fc8
hwansoo17 <playground99@naver.com>
2023-08-22 09:13:54 +0900

    로그인 화면 초안

--- Screen/AuthStackScreens/LoginScreen.js
+++ Screen/AuthStackScreens/LoginScreen.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, Text, View, TextInput, StyleSheet } from "react-native"
+import { SafeAreaView, Text,Image, View, TextInput, StyleSheet } from "react-native"
 import { Alert } from "react-native"
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler"
 import { useState, useContext } from "react"
@@ -71,84 +71,191 @@ const LoginScreen = ({ navigation }) => {
     }
     const styles = StyleSheet.create({
         container: {
-            flex: 1,
-            marginTop: 0,
-            backgroundColor: '#fff',
-            justifyContent: 'center',
-            paddingHorizontal: 20,
+            width: 390,
+            height: 844,
+            backgroundColor: '#ffffff'
         },
-        title: {
-            fontSize: 24,
-            fontWeight: 'bold',
-            textAlign: 'center',
-            marginBottom: 20,
-        },
-        input: {
+        podo: {
+            position: 'absolute',
+            width: 117,
             height: 40,
-            borderColor: '#ccc',
-            borderWidth: 1,
-            marginBottom: 10,
-            paddingHorizontal: 10,
-            borderRadius: 8,
-            marginHorizontal: 100
-        },
-        button: {
-            backgroundColor: '#4e9bde',
-            paddingVertical: 10,
-            borderRadius: 10,
-        },
-        buttonText: {
-            color: '#fff',
+            left: 137,
+            top: 124,
+            fontWeight: '800',
+            fontSize: 40,
+            lineHeight: 50,
             textAlign: 'center',
-            fontSize: 18,
+            letterSpacing: 0.59,
+            color: '#3BCDA1',
         },
+        postDonation: {
+            position: 'absolute',
+            width: 120,
+            height: 22,
+            left: 135,
+            top: 165,  // 프로젝트에 이 글꼴이 포함되어 있어야 합니다.
+            fontWeight: '700',
+            fontSize: 14,
+            lineHeight: 22,
+            textAlign: 'center',
+            letterSpacing: 0.59,
+            color: '#3BCDA1',
+        },
+        email: {
+            position: 'absolute',
+            height: 45,
+            left: '2.56%',  // 리액트 네이티브에서는 백분율로 위치를 지정할 수 있습니다.
+            right: '2.56%',
+            top: 246,
+            backgroundColor: '#F4F4F4',
+            borderColor: '#D9D9D9',
+            borderWidth: 1,
+            borderRadius: 8,
+        },
+        password: {
+            position: 'absolute',
+            height: 45,
+            left: '2.56%',  // 리액트 네이티브에서는 백분율로 위치를 지정할 수 있습니다.
+            right: '2.56%',
+            top: 329,
+            backgroundColor: '#F4F4F4',
+            borderColor: '#D9D9D9',
+            borderWidth: 1,
+            borderRadius: 8,
+        },
+        loginbox: {
+            position: 'absolute',
+            width: 370,
+            height: 45,
+            top: 412,
+            opacity: 0.5,
+        },
+
+        loginButton: {
+            height: 45,
+            left: '2.56%',
+            right: '2.56%',
+            backgroundColor: '#3BCDA1',
+            borderRadius: 8,
+            justifyContent: 'center', // 텍스트를 세로로 중앙에 위치시키기 위해 추가
+            alignItems: 'center',     // 텍스트를 가로로 중앙에 위치시키기 위해 추가
+        },
+        loginText: {
+            fontWeight: '700',
+            fontSize: 14,
+            lineHeight: 22,
+            letterSpacing: -0.408,
+            color: '#FFFFFF',
+            textAlign: 'center',
+        },
+        registerText: {
+            position: 'absolute',
+            left: '3.33%',
+            top: 477,
+            fontStyle: 'normal',
+            fontWeight: '400',
+            fontSize: 14,
+            color: '#000000',
+        },
+        kakaoButton: {
+            position: 'absolute',
+            width: 370,
+            height: 45,
+            top: 532,
+            
+        },
+        kakaoButton2: {
+            height: 45,
+            left: '2.56%',
+            right: '2.56%',
+            backgroundColor: '#FEE500',
+            borderRadius: 8,
+            alignItems: 'center', // 센터 정렬
+            justifyContent: 'center', // 센터 정렬
+        },
+        kakaoText: {
+
+            top: -1.5,
+            fontFamily: 'Pretendard',
+            fontStyle: 'normal',
+            fontWeight: '700',
+            fontSize: 14,
+            color: '#000000',
+        },
+        kakaoIcon: {
+            width: 16,
+            height: 15,
+            position: 'absolute',
+            left: 138,
+            top: 15, 
+        },
+        passwordText: {
+            position: 'absolute',
+            right: '2.56%',
+            top: 477,
+            fontStyle: 'normal',
+            fontWeight: '400',
+            fontSize: 14,
+            color: '#000000',
+        },
+
+    
+
+
+       
     });
     return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView>
-            <View>
-                <Stack.Screen name="회원가입" component={RegisterScreen} />
-                <TextInput
+        <View style={styles.container}>
+            <Text style={styles.podo}>
+                PODO
+            </Text>
+            <Text style={styles.postDonation}>
+                POST DONATION
+            </Text>
+        
+                <TextInput style={styles.email}
                     onChangeText={(userEmail) => { setUserEmail(userEmail) }}
                     placeholder='이메일'
-                    style={styles.input}
                 />
-                <TextInput
+            
+            
+                <TextInput style={styles.password}
                     onChangeText={(userPassword) => { setUserPassword(userPassword) }}
                     placeholder='비밀번호'
-                    style={styles.input}
                     secureTextEntry
                 />
-                <View style={{ flexDirection: 'row' }}>
-                    <TouchableOpacity
-                        onPress={() => { loginSubmit(); }}
-                        style={styles.button}
-                    >
-                        <Text style={styles.buttonText}>로그인</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate('RegisterScreen')}
-                        style={styles.button}
-                    >
-                        <Text style={styles.buttonText}>회원가입</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        onPress={() => {
-                            loginWithKakao()
-                        }}
-                        style={styles.button}
-                    >
-                        <Text style={styles.buttonText}>카카오로그인</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                    onPress={() => navigation.navigate('ForgotPasswordScreen')}
+        
+            <View style={styles.loginbox} >
+                <TouchableOpacity 
+                    style={styles.loginButton}
+                    onPress={() => { loginSubmit(); }}
                 >
-                    <Text>비밀번호 찾기</Text>
+                    <Text style={styles.loginText}>로그인</Text>
                 </TouchableOpacity>
-                </View>
             </View>
-            </ScrollView>
-        </SafeAreaView>
+                
+            
+            <View style={styles.registerText}>
+                <TouchableOpacity 
+                    onPress={() => navigation.navigate('RegisterScreen')}                    
+                >
+                    <Text>회원가입</Text>
+                </TouchableOpacity>
+            </View>
+            <View style ={styles.kakaoButton}>
+                <TouchableOpacity style={styles.kakaoButton2} onPress={() => loginWithKakao()}>
+                <Image source={require('C:/Users/playg/OneDrive/Desktop/paran/Gibu/images/KakaoTalk_logo.png')} style={styles.kakaoIcon} />
+                <Text style={styles.kakaoText}>       카카오 로그인</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.passwordText}>
+                <TouchableOpacity
+                onPress={() => navigation.navigate('ForgotPasswordScreen')}
+                >
+                <Text>비밀번호 찾기</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
     )
 }
 export default LoginScreen
