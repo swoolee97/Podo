09b03768a311b0a19a6c68bb0334a4610ea08615
조성주 <joseongju@joseongjuui-MacBookAir.local>
2023-08-25 00:34:11 +0900

    mission

--- App.js
+++ App.js
@@ -6,17 +6,23 @@ import Main from './Main';
 const Stack = createStackNavigator()
 import { enableScreens } from 'react-native-screens';
 import AuthStackScreen from './Screen/StackScreens/AuthStackScreen';
+import { MissionProvider } from './Screen/MissionStackScreens/MissionContents';
+import MissionStackScreen from './Screen/StackScreens/MissionStackScreen';
+
 enableScreens();
 const App = () => {
   return (
+    <MissionProvider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName='SplashScreen'>
           <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
           <Stack.Screen name="Main" component={Main} options={{ headerShown: false }} />
           <Stack.Screen name="Auth" component={AuthStackScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Mission" component={MissionStackScreen} options={{ headerShown: false }} />
         </Stack.Navigator>
       </NavigationContainer>
-  )
-}
+    </MissionProvider>  
+  );
+};
 
 export default App;
