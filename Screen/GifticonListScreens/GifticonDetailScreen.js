ba2cc194e4885271aa29b15ccde5db08cf12c80d
hwansoo17 <playground99@naver.com>
2023-09-10 20:32:03 +0900

    기프티콘세부화면

--- Screen/GifticonListScreens/GifticonDetailScreen.js
+++ Screen/GifticonListScreens/GifticonDetailScreen.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { SafeAreaView, View, Text, TextInput } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { SafeAreaView, View, Text} from "react-native";
+import { TouchableOpacity } from "react-native";
 import PreURL from "../../PreURL/PreURL";
 import { Image } from 'react-native'
+import styles from '../Styles/Styles.js';
+import favoriteImage from '../../images/Favorite.png';
+import heartImage from '../../images/Heart2.png';
 
 const GifticonDetailScreen = ({ navigation, route }) => {
     const gifticonId = route.params.gifticonId;
     const [gifticon, setGifticon] = useState(null);
+    const [liked, setLiked] = useState(false);  // 좋아요 버튼 상태 관리
+
+    const toggleLike = () => {
+        setLiked(!liked);
+    };
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch(PreURL.preURL + `/api/gifticon/detail?id=${gifticonId}`)
@@ -16,40 +24,68 @@ const GifticonDetailScreen = ({ navigation, route }) => {
         fetchData();
     })
     return (
-        <SafeAreaView>
-            <View>
-                <Text>기프티콘 상세(사진, 포인트, 유효기간 등)</Text>
-            </View>
-            <View>
+        
+        <SafeAreaView style={[styles.container]}>
+            <View style={{flex:9}}>
                 {gifticon ? (
-                    <View>
-                        <Image 
-                            source={{ uri: gifticon.url }} 
-                            style={{ width: 200, height: 200 }}
-                        />
-                        <Text>{gifticon.gifticon_name}</Text>
-                        <Text>{gifticon.price}원</Text>
-                        <Text>{gifticon.company}</Text>
-                        <Text>{gifticon.category}</Text>
+                    <View style={{marginTop: '10%'}}>
+                        <View  style={{width:'60%', left: '20%'}}>
+                            <Image 
+                                source={{ uri: gifticon.url }} 
+                                style={styles.image}
+                            />
+                        </View>
+                        <TouchableOpacity style={{margin: '5%', width: '28%',height:27,justifyContent: 'center', alignItems:'center',borderWidth: 1, borderColor: '#c9c9c9', borderRadius: 15}}>
+                            <Text style={[styles.PretendardRegular,{fontSize: 12, color: '#000000'}]}>{gifticon.company}</Text>
+                        </TouchableOpacity>
+                        <Text style={ {fontFamily: 'Pretendard-Regular', marginLeft: '5%', fontSize: 16, color: '#000000'}}>{gifticon.gifticon_name}</Text>
+                        <View style={{margin: '5%', width: '90%',height:65,justifyContent: 'center', alignItems:'center',borderWidth: 1, borderColor: '#c9c9c9', borderRadius: 10}}>    
+                            <Text style={ {fontFamily: 'Pretendard-Medium', fontSize: 18, color: '#000000'}}>
+                                {gifticon.price}포인트
+                            </Text>
+                        </View>
+                        
                     </View>
-                ) : (<Text>asdf</Text>)}
+                ) : (<Text>asd</Text>)}
             </View>
-            <View>
-                <TouchableOpacity onPress={() => {
-                    navigation.navigate('PurchaseScreen',{
-                        gifticon
-                    })
-                }}><Text>구매하기</Text>
+            <View
+            style={{height: 1, width: "100%", backgroundColor: "#c9c9c9"}}/>
+            <View style={{ flex: 1, flexDirection: 'row',alignItems: 'center', justifyContent:'center', margin:'1%'}}>
+                <TouchableOpacity
+                    onPress={toggleLike} 
+                    style={{
+                        
+                        aspectRatio: 1,
+                        height: '70%',
+                        borderColor: "#c9c9c9",
+                        borderWidth:1,
+                        borderRadius: 8,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        
+                    }}
+                >
+                    <Image 
+                        source={liked ? heartImage : favoriteImage } 
+                        style={{aspectRatio:1, height:'60%'}}                    
+                    />
                 </TouchableOpacity>
-            </View>
-            <View>
-                <Text>장바구니에 담기</Text>
-            </View>
-            <View>
-                <Text>(공유하기)</Text>
-            </View>
-            <View>
-                <Text>(선물하기)</Text>
+
+                <TouchableOpacity onPress={() => {
+                    navigation.navigate('PurchaseScreen',{gifticon})
+                    }
+                }
+                style={{
+                marginLeft:'4%',
+                width:'75%',
+                height: '70%',
+                backgroundColor: '#3BCDA1',
+                borderRadius: 8,
+                justifyContent: 'center', // 텍스트를 세로로 중앙에 위치시키기 위해 추가
+                alignItems: 'center', }}
+                >
+                    <Text style={styles.buttonText}>구매하기</Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     )
