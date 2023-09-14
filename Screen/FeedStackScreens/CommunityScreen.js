9c34433ad6e94631216f72a6a359504719d2465d
hwansoo17 <playground99@naver.com>
2023-09-14 16:52:58 +0900

    피드 디자인 완료

--- Screen/FeedStackScreens/CommunityScreen.js
+++ Screen/FeedStackScreens/CommunityScreen.js
@@ -1,39 +1,12 @@
 import React, { useState, useEffect} from 'react';
-import { View, FlatList, Text, Image, Button, ScrollView,Dimensions } from 'react-native';
+import { View, FlatList, Text, Image, Button, ScrollView,Dimensions, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StyleSheet } from 'react-native';
 import PreURL from '../../PreURL/PreURL';
+import styles from '../Styles/Styles.js';
 
 const { width } = Dimensions.get('window');
 const itemWidth = width / 2-10;
-
-const styles = StyleSheet.create({
-  imageContainer: {
-    padding: 5,
-  },
-  postContainer: {
-    marginBottom: 20,
-    width: itemWidth,
-  },
-  imageStyle: {
-    width: itemWidth,
-    height: 150,
-  },
-  columnWrapperStyle: {
-    justifyContent: 'space-between',
-  },
-  listItem: {
-    width: '45%',
-    marginTop: '5%'
-  },
-  image: {
-    aspectRatio: 1,
-    borderRadius: 15,
-    borderColor: '#A1A1A1',
-    borderWidth: 1,
-  },
-});
-
 const CommunityScreen = () => {
   const [posts, setPosts] = useState([]);
   const navigation = useNavigation();
@@ -69,34 +42,55 @@ const CommunityScreen = () => {
   };
 
   return (
-    <View style={{ flex: 1, padding: isLargeScreen ? 40 : 20 }}>
-      <FlatList
-        data={posts}
-        keyExtractor={(item) => item._id ? item._id.toString() : Math.random().toString()}
-        renderItem={({ item }) => (
-          <View style={styles.postContainer}>
-            <Text>{item.email}</Text>
-            <Text style={{ fontSize: 8 }}>작성 시간: {new Date(item.createdAt).toLocaleString()}</Text>
-            {Array.isArray(item.imageUrl) && (
-              <ScrollView 
-                horizontal={true} 
-                pagingEnabled={true} 
-                showsHorizontalScrollIndicator={false}
-              >
-                {item.imageUrl.map((uri, index) => (
-                      <View style={styles.imageContainer} key={index}>
-                        <Image source={{ uri }} style={styles.imageStyle}/>
-                      </View>
-                ))}
-              </ScrollView>
-            )}
-            <Text onPress={() => goToPostDetail(item)}>{item.text}</Text>
-          </View>
-        )}
-        numColumns={isLargeScreen ? 3 : 2}
-        columnWrapperStyle={styles.columnWrapperStyle}
-      />
-       <Button title="글쓰기" onPress={() => navigation.navigate('WriteCommunity')} />
+    <View style={styles.container}>
+      <View style={{flex:9}}>
+        <FlatList
+          style={{margin: '5%'}}
+          columnWrapperStyle={styles.columnWrapper}
+          data={posts}
+          keyExtractor={(item) => item._id ? item._id.toString() : Math.random().toString()}
+          renderItem={({ item }) => (
+            <View style={styles.listItem}>
+              <TouchableOpacity  onPress={() => goToPostDetail(item)}>
+                {Array.isArray(item.imageUrl) && (
+                  <ScrollView 
+                    horizontal={true} 
+                    pagingEnabled={true} 
+                    showsHorizontalScrollIndicator={false}
+                  >
+                    {item.imageUrl.map((uri, index) => (
+                          <View style={styles.imageContainer} key={index}>
+                            <Image source={{ uri }} style={styles.image}/>
+                          </View>
+                    ))}
+                  </ScrollView>
+                )}
+                <Text style={styles.itemName}>{item.text}</Text>
+              </TouchableOpacity>
+              <Text  style={styles.price}>{item.email}</Text>
+              <Text style={styles.brandtext}>작성 시간: {new Date(item.createdAt).toLocaleString()}</Text>
+            </View>
+          )}
+          numColumns={2}
+        />
+      </View>
+      <View style={{flex:1, justifyContent: 'center', // 텍스트를 세로로 중앙에 위치시키기 위해 추가
+          alignItems: 'center'}}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('WriteCommunity')}
+          style={{
+            marginHorizontal: '5%',
+            width: '90%',
+            height: '70%',
+            backgroundColor: '#3BCDA1',
+            borderRadius: 8,
+            justifyContent: 'center', // 텍스트를 세로로 중앙에 위치시키기 위해 추가
+            alignItems: 'center'}}
+        >
+          <Text style={styles.buttonText}>글쓰기</Text>  
+        
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
