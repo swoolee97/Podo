b8db101458101dc38cbda3f58ea13043563e4091
조성주 <joseongju@joseongjuui-MacBookAir.local>
2023-10-03 13:04:22 +0900

    성주 에러 해결

--- PreURL/PreURL.js
+++ PreURL/PreURL.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // preURL: 'http://34.232.120.99:3001' // 배포서버.
-    preURL : 'http://localhost:3001' // 로컬서버. 테스트할 땐 이거 써야함.
+    preURL: 'http://34.232.120.99:3001' // 배포서버.
+    // preURL : 'http://localhost:3001' // 로컬서버. 테스트할 땐 이거 써야함.
     // preURL : 'http://10.0.2.2:3001' 
 };
