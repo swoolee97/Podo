a36bde0e4a341c0cd2bef2feccee5f388a70304f
조성주 <joseongju@joseongjuui-MacBookAir.local>
2023-11-14 21:08:08 +0900

    마이페이지

--- Screen/MyPageStackScreens/Profile.js
+++ Screen/MyPageStackScreens/Profile.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { View, Text, Image, TextInput, Alert, Button} from "react-native";
+import { View, Text, Image, TextInput, Alert, Button, FlatList} from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { useIsFocused } from '@react-navigation/native';
 import { TouchableOpacity } from "react-native";
 import PreURL from '../../PreURL/PreURL';
 import styles from '../Styles/Styles';
 import { SafeAreaView } from "react-native-safe-area-context";
+import profile1 from '../../images/profile1.png'
+import profile2 from '../../images/profile2.png'
+import profile3 from '../../images/profile3.png'
+import profile4 from '../../images/profile4.png'
 
 const Stack = createStackNavigator()
 
@@ -20,6 +24,7 @@ const Profile = ({ navigation }) => {
     const [inputValue, setInputValue] = useState('');
     const [showCheck, setShowCheck] = useState(false);
     const [selectedProfileImage, setSelectedProfileImage] = useState(null);
+    const profileImages = [profile1,profile2,profile3,profile4]
 
     const handleChangeText = (text) => {
         setInputValue(text);
@@ -33,76 +38,55 @@ const Profile = ({ navigation }) => {
     const renderCheckmark = (imageName) => {
         if (selectedButton == imageName) {
             return (
-                <View style={{width:18, height: 18, alignSelf: 'center', backgroundColor: '#4577FA', borderRadius:30, alignItems: 'center' ,justifyContent: 'center'}}>
-                    <Image source={require('../../images/check.png')} style={{width: 12, height: 12}} />
+                <View style={{width:'15%', aspectRatio:1, alignSelf: 'center', backgroundColor: '#4577FA', borderRadius:30, alignItems: 'center' ,justifyContent: 'center'}}>
+                    <Image source={require('../../images/check.png')} style={{aspectRatio:1, height:'80%'}} />
                 </View>
             );
         }
         return null;
     };
 
+    const renderItem = ({ item, index }) => (
+        <View style ={{width: '45%', aspectRatio: 1,alignItems: 'center'}}>
+            <TouchableOpacity onPress={() => { setSelectedButton(`프로필 사진${index + 1}`); setSelectedProfileImage(item) }} style = {{aspectRatio: 1, width: '80%', backgroundColor:'#DBFFF2', borderRadius: 20, alignItems: 'center'}}>
+
+                    <Image source={item} style={{aspectRatio: 1, flex: 1, borderRadius: 40}} />
+
+            </TouchableOpacity>
+            {renderCheckmark(`프로필 사진${index + 1}`)}
+        </View>
+      );
+
     return(
-        <SafeAreaView style={styles.container}>
-            <View style={{alignItems: 'center'}}>
-                <View style={{alignItems: 'center'}}>
-                <Text style={{fontSize: 14, fontFamily: 'Pretendard-Medium', color: '#000'}}>
+        <View style={styles.container}>
+            <View style = {{flex:1}}/>
+                    
+                <Text style={{fontSize: 14, fontFamily: 'Pretendard-Medium', color: '#000', alignSelf: 'center'}}>
                         프로필과 닉네임 수정이 가능합니다.
-                    </Text>
-                <View style={{ width: 100, height: 100, borderRadius: 50, backgroundColor: '#D9D9D9', justifyContent: 'center', alignItems: 'center',alignSelf: 'center' }}>
-                    {selectedProfileImage && <Image source={selectedProfileImage} style={{ width: 100, height: 100, borderRadius: 100 }} />}
+                </Text>
+                <View style = {{flex:0.5}}/>
+                <View style={{ aspectRatio: 1, width: '30%', borderRadius: 100, backgroundColor: '#D9D9D9', justifyContent: 'center', alignItems: 'center',alignSelf: 'center'}}>
+                    {selectedProfileImage && <Image source={selectedProfileImage} style={{ aspectRatio: 1, flex: 1, borderRadius: 100}} />}
                 </View>
+                <View style = {{flex:1}}/>
+                <View style={{alignItems: 'center'}}>
+                <FlatList
+                    style = {{}}
+                    data={profileImages}
+                    renderItem={renderItem}
+                    keyExtractor={(index) => `key-${index}`}
+                    horizontal={false}
+                    numColumns={2} // 한 줄에 이미지 두 개씩 표시
+                />
                 </View>
-
-                <View style={{flexDirection: 'row', marginTop: 60}}>
-                    <TouchableOpacity
-                        style={{margin: '5%'}}
-                        onPress={() => {setSelectedButton('프로필 사진1');setSelectedProfileImage(require('../../images/point_grape.png'))} }
-                    >
-                        <View style={{width:112, height:112, backgroundColor:'#DBFFF2', borderRadius: 20, alignItems: 'center', justifyContent: 'center', paddingBottom: 15}}>
-                            <Image source={require('../../images/point_grape.png')} style={{width: 150, height: 150, borderRadius: 40}} />
-                            {renderCheckmark('프로필 사진1')}
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        style={{margin: '5%'}}
-                        onPress={() => {setSelectedButton('프로필 사진2');setSelectedProfileImage(require('../../images/profile2.png'))}}                
-                    >
-                        <View style={{width:112, height:112, backgroundColor:'#EBEBEB', borderRadius: 20, alignItems: 'center', justifyContent: 'center', paddingBottom: 15}}>
-                            <Image source={require('../../images/profile2.png')} style={{width: 150, height: 150, borderRadius: 40}} />
-                            {renderCheckmark('프로필 사진2')}
-                        </View>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={{flexDirection: 'row', marginBottom: 50}}>
-                    <TouchableOpacity
-                        style={{margin: '5%', marginTop:'10%'}}
-                        onPress={() => {setSelectedButton('프로필 사진3');setSelectedProfileImage(require('../../images/profile3.png'))}}                
-                    >
-                        <View style={{width:112, height:112, backgroundColor:'#E3F7FE', borderRadius: 20, alignItems: 'center', justifyContent: 'center', paddingBottom: 15}}>
-                            <Image source={require('../../images/profile3.png')} style={{width: 150, height: 150, borderRadius: 40}} />
-                            {renderCheckmark('프로필 사진3')}
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        style={{margin: '5%', marginTop:'10%'}}
-                        onPress={() => {setSelectedButton('프로필 사진4');setSelectedProfileImage(require('../../images/profile4.png'))}}                
-                    >
-                        <View style={{width:112, height:112, backgroundColor:'#FFF7EC', borderRadius: 20, alignItems: 'center', justifyContent: 'center', paddingBottom: 15}}>
-                            <Image source={require('../../images/profile4.png')} style={{width: 150, height: 150, borderRadius: 40}} />
-                            {renderCheckmark('프로필 사진4')}
-                        </View>
-                    </TouchableOpacity>
-                </View>
+                <View style = {{flex:1}}/>
 
                 <TextInput
-                    placeholder="닉네임을 입력하세요"
+                    placeholder="수정할 닉네임을 입력하세요"
                     placeholderTextColor={'#B9B9B9'}
                     value={inputValue}
                     onChangeText={handleChangeText} // 입력된 값으로 상태 업데이트
-                    style={{width: 200, height: 50, borderBottomWidth: 1, textAlign:'center', fontFamily:'Pretendard-Medium', fontSize:18, color:'#000'}}
+                    style={{width: '50%', borderBottomWidth: 1, textAlign:'center', fontFamily:'Pretendard-Medium', fontSize:18, color:'#000', alignSelf:'center'}}
                 />
                 {inputValue.length === 0 ? (
                     // 입력값이 없을 때
@@ -110,20 +94,17 @@ const Profile = ({ navigation }) => {
                 ) : showCheck ? (
                     // 입력값이 4글자 이상일 때a
                     <View style={{height:52, justifyContent:'center'}}>
-                        <View style={{width:18, height: 18, alignSelf: 'center', backgroundColor: '#4577FA', borderRadius:30, alignItems: 'center' ,justifyContent: 'center', marginVertical:7.5}}>
+                        <View style={{width:18, height: 18, alignSelf: 'center', backgroundColor: '#4577FA', borderRadius:30, alignItems: 'center' ,justifyContent: 'center'}}>
                             <Image source={require('../../images/check.png')} style={{width: 12, height: 12}} />
                         </View>
                         <Text style={{fontFamily: 'Pretendard-Medium', fontSize: 14, color: '#B9B9B9'}}>사용하실 수 있는 이름이에요.</Text>
                     </View>
                 ) : (
                     // 입력값이 있지만 4글자 미만일 때
-                    <View style={{height:52, justifyContent:'center'}}>
+                    <View style={{height:'5%', justifyContent:'center', alignSelf: 'center'}}>
                         <Text style={{fontFamily: 'Pretendard-Medium', fontSize: 14, color: '#FF7474'}}>닉네임은 4글자 이상으로 해주세요.</Text>
                     </View>
                 )}
-
-
-            </View>
             <View style={{flex:1,justifyContent:'center', alignItems:'center'}}>
             <Button
             style={{}}
@@ -141,7 +122,8 @@ const Profile = ({ navigation }) => {
             }}  
             />
             </View>
-        </SafeAreaView>
+            <View style = {{flex:1}}/>
+        </View>
     )
 }
 export default Profile
